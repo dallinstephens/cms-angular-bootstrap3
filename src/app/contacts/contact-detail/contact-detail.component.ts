2e028df2b0089f6deb89e832790b542d20b369fe
DallinS <dallinstephens1@gmail.com>
2026-01-17 17:38:55 -0700

    cms W02 Assignment complete

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrl: './contact-detail.component.css'
+})
+export class ContactDetailComponent {
+  contact: Contact;
+
+  ngOnInit() {
+    // this.contact = new Contact(
+    //   '1',
+    //   'R. Kent Jackson',
+    //   'jacksonk@byui.edu',
+    //   '208-496-3771',
+    //   '../../assets/images/jacksonk.jpg',
+    //   null
+    // );
+  }
+}
