b3723435ac02ba3291ae9b8583a44e6eeb72c070
DallinS <dallinstephens1@gmail.com>
2026-01-31 16:03:29 -0700

    W04 Assignment - Documents and Header User Dropdown

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Document } from '../document.model';
 
 @Component({
@@ -7,5 +7,28 @@ import { Document } from '../document.model';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
-  documents: Document[] = [];
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    // id, name, description, url, children
+    new Document('1', 'CIT 111 - Introduction to Databases', 'MySQL, MySQL Workbench', '../../assets/images/documentCIT111.jpg', null),
+    new Document('2', 'CSE 111 - Programming with Functions', 'Python3, Visual Studio Code', '../../assets/images/documentCSE111.jpg', null),
+    new Document('3', 'CSE 210 - Programming with Classes', 'C#, Visual Studio Code, Classes', '../../assets/images/documentCSE210.jpg', null),
+    new Document('4', 'CSE 212 - Programming with Data Structures', 'C#, Visual Studio Code, Data Structures', '../../assets/images/documentCSE212.jpg', null),
+    new Document('5', 'WDD 130 - Web Fundamentals', 'HTML, CSS, Wireframes', '../../assets/images/documentWDD130.jpg', null)
+  ];
+
+  onSelectedDocument(document: Document) {
+    // Reference: https://byui.instructure.com/courses/404738/pages/w04-assignment-instructions
+    // emit the selectedDocumentEvent and pass it the document object passed into the method
+    this.selectedDocumentEvent.emit(document);
+  }
 }
+
+// Reference: https://gemini.google.com/app
+// Explanation:
+// The Trigger: A user clicks a document in your HTML template.
+// The Method: That click calls onSelectedDocument(document).
+// The Line: Your line this.selectedDocumentEvent.emit(document) sends that document out of this component.
+// The Parent: Your parent component (likely DocumentsComponent) will be "listening" in its HTML like this: 
+// <cms-document-list (selectedDocumentEvent)="doSomething($event)"></cms-document-list>
