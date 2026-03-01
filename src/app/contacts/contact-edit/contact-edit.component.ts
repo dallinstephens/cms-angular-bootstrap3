9d953b7122e1814cb35cb8555e2c4f9d7c912e8b
DallinS <dallinstephens1@gmail.com>
2026-02-28 22:24:24 -0700

    Week 8 Angular Forms

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+import { NgForm } from '@angular/forms';
+import { ContactService } from '../contact.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
-export class ContactEditComponent {
+export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = params['id'];
+          if (this.id === undefined || this.id === null) {
+            this.editMode = false;
+            return;
+          }
+          this.originalContact = this.contactService.getContact(this.id);
+          if (this.originalContact === undefined || this.originalContact === null) {
+            return;
+          }
+          this.editMode = true;
+          this.contact = JSON.parse(JSON.stringify(this.originalContact));
+          if (this.contact.group) {
+            this.groupContacts = JSON.parse(JSON.stringify(this.contact.group));
+          }
+        }
+      );
+  }
+
+
+  onSubmit(form: NgForm) {
+    const value = form.value; // gets values from form's fields
+    let newContact = new Contact(
+      '', // id
+      value.name,
+      value.email,
+      value.phone,
+      value.imageUrl,
+      null
+    );
+
+    if (this.editMode === true) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+
+    this.router.navigate(['/contacts']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
 }
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'cms-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit {
+  @ViewChild('f') documentForm: NgForm;
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+  id: string;
 
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.
+      subscribe(
+        (params: Params) => {
+          this.id = params['id'];
+          if (this.id === undefined || this.id === null) {
+            this.editMode = false;
+            return;
+          }
+          this.originalDocument = this.documentService.getDocument(this.id);
+          if (this.originalDocument === undefined || this.originalDocument === null) {
+            return;
+          }
+          this.editMode = true;
+          // Reference: https://byui.instructure.com/courses/404738/pages/w08-assignment-instructions
+          // The original JSON object stored in the originalDocument property is passed to the JSON stringify() method. 
+          // It returns a string representation of the JSON object. Then, we call the JSON parse() method and pass it 
+          // the string returned from the stringify() method. The parse() method creates and returns a new JSON object 
+          // based on the string passed to it.
+          this.document = JSON.parse(JSON.stringify(this.originalDocument));
+          // Here is another way below to do the above line of code.
+          // this.document = { ...this.originalDocument };        
+        }
+      );
+  }
+
+  // Reference: https://byui.instructure.com/courses/404738/pages/w08-assignment-instructions
+  // This method is called when the end user selects the Save button and submits the form. 
+  // It is responsible for either adding a new document to the document list or updating an 
+  // existing document in the document list.
+  onSubmit(form: NgForm) {
+    // console.log(form);
+    const value = form.value; // gets values from form's fields
+    let newDocument = new Document(
+      '', // id
+      value.name,
+      value.description,
+      value.url,
+      null
+    );
+
+    if (this.editMode === true) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+
+    this.router.navigate(['/documents']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
+  }
 }
